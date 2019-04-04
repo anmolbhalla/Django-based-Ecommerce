2db6ed6be68b1b94307cf4413ea9305b1f4712b1
aryansharma23124 <aryansharma23124@gmail.com>
2019-04-04 19:56:04 +0530

    Cart and Invoice Commit

--- Ecommerce/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import userAccounts
-
-admin.site.register(userAccounts)
-# Register your models here.
--- Ecommerce/accounts/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class userAccounts(models.Model):
-    userName=models.CharField(max_length=50)
-    email=models.CharField(max_length=50)
-    password=models.CharField(max_length=50)
